43b2a1e4c0bae3e822540eae8d0329055c838f38
Matteo Ghilardini <matteo.ghilardini@gmail.com>
2024-09-24 15:47:11 +0200

    trying fix cors

--- src/store/services/userService.ts
+++ src/store/services/userService.ts
@@ -1,8 +1,11 @@
 const BASE_URL = process.env.REACT_APP_API_ROUTE;
 
 export const checkHealth = async () => {
-  const response = await fetch(`${BASE_URL}/health`);
-  return response;
+  const response = await fetch(`${BASE_URL}/health`, {
+    method: 'GET',
+    mode: 'cors', // Enable CORS
+    credentials: 'include', // If you're sending cookies/auth tokens
+  });
 };
 
 // export const getAccessToken = async (username: string, password: string) => {
